2122bf8f1b93c13a8c5f797ab86e0f5dab7fab44
Jacquelyne Glover <jackmidoglover@gmail.com>
2018-12-15 22:48:44 -0700

    wtf

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const Header = props => {
+    return(
+        <header className="container center">
+        <h1 id="logo" className="white-text">Moon Child</h1>
+        </header>
+    )
+};
+
+export default Header;
