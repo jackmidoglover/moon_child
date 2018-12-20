eccb9b2844aa211bec42f7ae10fc1422b0d51761
Jacquelyne Glover <jackmidoglover@gmail.com>
2018-12-19 23:53:35 -0700

    Phaser image component successfully working!

--- client/src/components/phaseIndicator/Phaser.jsx
+++ client/src/components/phaseIndicator/Phaser.jsx
@@ -7,7 +7,7 @@ import moment from 'moment';
 export class Phaser extends React.Component {
     state = {
         latitude: 39.8097,
-        longitude: 98.5556,
+        longitude: -98.5556,
         date: moment().format("YYYYMMDD"),
         timezone: -6,
         moonPhase: '',
@@ -17,9 +17,57 @@ export class Phaser extends React.Component {
         phaserClass: ''
     }
 
+    phaserClassSet = (phase, illumination) => {
+        // Condition to check gibbous
+        if (.97 >= illumination >= .67){
+            if (phase === "Waxing Gibbous"){
+            return "gibbousWax";
+            }
+            else if (phase === "Waning Gibbous"){
+                return "gibbousWane";
+            }
+        }
+        // Condition to check for full
+        else if (illumination >= .98){
+            return "fullMoon";
+        }
+        // Condition to check for half moon wane or wax
+        else if ( .66 >= illumination >= .44){
+            if (phase === "Waxing Crescent" || phase === "Waxing Crescent"){
+                return "waxHalfMoon";
+            }
+            else if (phase === "Waning Gibbous" || phase === "Waning Crescent"){
+                return "waneHalfMoon";
+            }
+        }
+        // condition to check for crescent (larger)
+        else if (.43 >= illumination >= .15){
+            if (phase === "Waning Crescent"){
+            return "waneCrescent";
+            }
+            else if (phase === "Waxing Crescent"){
+                return "waxCrescent";
+            }
+        }
+        // condition to check for crescent (smaller)
+        else if (.11 >= illumination >= .0016){
+            if (phase === "Waning Crescent"){
+                return "sliverWane";
+            }
+            else if (phase === "Waxing Crescent"){
+                return "sliverWax";
+            }
+        }
+        // condition to check for new moon
+        else if (.0015 >= illumination){
+            return "newMoon";
+        }
+    }
+
     componentWillMount(){
         console.log(this.state.date);
         api.querySolunar(this.state.latitude, this.state.longitude, this.state.date, this.state.timezone).then(moonData => {
+            console.log(moonData);
             const moonRise = moonData.data.moonRise;
             const moonSet = moonData.data.moonSet;
             const moonIllumination = moonData.data.moonIllumination;
@@ -28,7 +76,8 @@ export class Phaser extends React.Component {
                 moonPhase: moonPhase,
                 moonRise: moonRise, 
                 moonSet: moonSet,
-                moonIllumination: moonIllumination
+                moonIllumination: moonIllumination,
+                phaserClass: this.phaserClassSet(moonPhase, moonIllumination)
             });
             console.log(this.state);
         })
@@ -38,7 +87,7 @@ export class Phaser extends React.Component {
         return(
             <div>
                 <div className="moonContainer">
-                    <div className="gibbousWax"></div>
+                    <div className={this.state.phaserClass}></div>
                 </div>
             </div>
         )
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,13 +3,13 @@ const axios = require('axios');
 const solunarURL = "https://api.solunar.org/solunar/";
 
 export default {
-    querySolunar(latitude, longitude, date, z){
+    querySolunar(latitude,longitude,date,z){
         if(!latitude || !longitude || !date || !z ){
             throw new Error("Must have query paramters: latitude, longitude, date, timezone");
         } else if (isNaN(parseFloat(latitude)) || isNaN(parseFloat(longitude)) || isNaN(parseFloat(date)) || isNaN(parseFloat(z))){
             throw new Error("Query parameters must be a number or a number string");
         }
-       return axios.get(solunarURL + `${latitude}, ${longitude}, ${date}, ${z}`);
+       return axios.get(solunarURL + `${latitude},${longitude},${date},${z}`);
 
     }
 };
