3c9c00240efe6f413cedd7a22c54a8a3a76fd0c8
Jacquelyne Glover <jackmidoglover@gmail.com>
2018-12-19 22:51:19 -0700

    phaser component correctly making api call and setting state

--- client/src/components/phaseIndicator/Phaser.jsx
+++ client/src/components/phaseIndicator/Phaser.jsx
@@ -6,21 +6,39 @@ import moment from 'moment';
 
 export class Phaser extends React.Component {
     state = {
-        latitude: '',
-        longitude: '',
-        date: moment(),
-        timezone: '',
+        latitude: 39.8097,
+        longitude: 98.5556,
+        date: moment().format("YYYYMMDD"),
+        timezone: -6,
         moonPhase: '',
         moonRise: '',
-        moonSet: ''
+        moonSet: '',
+        moonIllumination: '',
+        phaserClass: ''
+    }
+
+    componentWillMount(){
+        console.log(this.state.date);
+        api.querySolunar(this.state.latitude, this.state.longitude, this.state.date, this.state.timezone).then(moonData => {
+            const moonRise = moonData.data.moonRise;
+            const moonSet = moonData.data.moonSet;
+            const moonIllumination = moonData.data.moonIllumination;
+            const moonPhase = moonData.data.moonPhase;
+            this.setState({
+                moonPhase: moonPhase,
+                moonRise: moonRise, 
+                moonSet: moonSet,
+                moonIllumination: moonIllumination
+            });
+            console.log(this.state);
+        })
     }
 
     render(){
         return(
             <div>
                 <div className="moonContainer">
-                    <div className="waneCrescent">
-                    </div>
+                    <div className="gibbousWax"></div>
                 </div>
             </div>
         )
