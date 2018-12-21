6080451e73463799291d979066ea055de901abd4
Jacquelyne Glover <jackmidoglover@gmail.com>
2018-12-20 23:33:49 -0700

    sign in and sign up buttons created and styled.. next up, a function for when they are clicked!

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/Header';
-import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../node_modules/materialize-css/dist/css/materialize.css';
+import '../node_modules/materialize-css/sass/components/_icons-material-design.scss';
 import Phaser from './components/phaseIndicator/Phaser';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-          <Header />
-          <Phaser />
+        <Header />
+        <Phaser />
+        <div className="row">
+          <div className="col m6 offset-m3 center">
+            <button class="btn waves-effect waves-light amber btnM"> Sign In  <i class="fas fa-sign-in-alt"></i>
+            </button>
+            <button class="btn waves-effect waves-light amber btnM">Sign Up  <i class="fas fa-user-plus"></i>
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
